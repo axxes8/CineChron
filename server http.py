976736c1344ba83392e6f040b3a77f198ec8d8aa
havens5 <77998742+havens5@users.noreply.github.com>
2022-05-16 14:57:34 -0600

    added classes and interactions with movie api via threading

--- server http.py
+++ server http.py
@@ -1,6 +1,14 @@
 import http.server
 import socketserver
 import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import datetime
+import json
+import time
+import random
+import threading
+import requests
 
 PORT = 8000
 
@@ -11,3 +19,113 @@ Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
+
+api_key = '9ba37aca04338a3886c632201a0a7dce'
+Base_URL = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query="
+Test_movie = "Jack reacher"
+
+class Movie:
+    
+    def __init__(self, json):
+        super().__init__()
+
+    def add_title(self, id):
+        pass
+
+    def get_details(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class TV_episode:
+    
+    def __init__(self, json):
+        super().__init__()
+
+    def add_season(self, id):
+        pass
+
+    def add_edisode(self, id):
+        pass
+
+    def get_details(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class TV_Show:
+    
+    def __init__(self, json):
+        super().__init__()
+
+    def add_title(self, id):
+        pass
+
+    def get_details(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Movie_Series:
+    
+    def __init__(self, json):
+        super().__init__()
+
+    def add_movie(self, id):
+        pass
+
+    def get_details(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class TV_Series:
+    
+    def __init__(self, json):
+        super().__init__()
+
+    def add_episode(self, id):
+        pass
+
+    def get_details(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class API_request(threading.Thread):
+    def __init__(self,url):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.response = {}
+
+    def run(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            self.response = response.json()
+        else:
+            print("Error retrieveing data on ",self.url)
+
+def get_movie(name):
+    url = Base_URL + name
+    json = API_request(url)
+    json.start()
+    json.join()
+
+    return json.response
+
+
+def main():
+    data = get_movie(Test_movie)
+
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
