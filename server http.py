25c363adbd508e97b3d6c63ad8832b511b906f54
havens5 <77998742+havens5@users.noreply.github.com>
2022-05-16 22:08:44 -0600

    Added and removed some classes and api stuff

--- server http.py
+++ server http.py
@@ -5,19 +5,20 @@ import json
 import threading
 import requests
 
-PORT = 8000
-
-web_dir = os.path.join(os.path.dirname(__file__), 'D:/Users/Taylor/Desktop')
-os.chdir(web_dir)
-
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
+#PORT = 8000
+#
+#web_dir = os.path.join(os.path.dirname(__file__), 'D:/Users/Taylor/Desktop')
+#os.chdir(web_dir)
+#
+#Handler = http.server.SimpleHTTPRequestHandler
+#httpd = socketserver.TCPServer(("", PORT), Handler)
+#print("serving at port", PORT)
+#httpd.serve_forever()
 
 api_key = '9ba37aca04338a3886c632201a0a7dce'
-Base_URL = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query="
-Test_movie = "Jack reacher"
+Movie_search = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query="
+Genre = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=en-US"
+Test_movie = "F9"
 
 class Movie:
     
@@ -65,35 +66,6 @@ class TV_Show:
         pass
 
 
-class Movie_Series:
-    
-    def __init__(self, json):
-        super().__init__()
-
-    def add_movie(self, id):
-        pass
-
-    def get_details(self):
-        pass
-
-    def __str__(self):
-        pass
-
-class TV_Series:
-    
-    def __init__(self, json):
-        super().__init__()
-
-    def add_episode(self, id):
-        pass
-
-    def get_details(self):
-        pass
-
-    def __str__(self):
-        pass
-
-
 class API_request(threading.Thread):
     def __init__(self,url):
         threading.Thread.__init__(self)
@@ -103,17 +75,22 @@ class API_request(threading.Thread):
     def run(self):
         response = requests.get(self.url)
         if response.status_code == 200:
-            self.response = response.json()
+            movie_search = response.json()
+            self.response = movie_search[1]
         else:
             print("Error retrieveing data on ",self.url)
 
-def get_movie(name):
-    url = Base_URL + name
+def get_movie(name,movie_file):
+    url = Movie_search + name
     json = API_request(url)
     json.start()
     json.join()
 
     return json.response
+
+def get_tv_show(name,tv_file):
+    url = Movie_search + name
+    pass
 
 
 def main():
